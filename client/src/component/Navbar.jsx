6d52a23106fae8043883609425c230e5d00d61c9
ujwal282 <ujwalpaudel65@gmail.com>
2025-03-19 16:12:04 +0545

    added navbar component

---
+++ client/src/component/Navbar.jsx
@@ -0,0 +1,22 @@
+const Navbar = () => {
+    return (
+        <>
+            <ul>
+                <a href="">
+                    <li>Home</li>
+                </a>
+                <a href="">
+                    <li>About</li>
+                </a>
+                <a href="">
+                    <li>Services</li>
+                </a>
+                <a href="">
+                    <li>Contact</li>
+                </a>
+            </ul>
+        </>
+    );
+}
+
+export default Navbar;
